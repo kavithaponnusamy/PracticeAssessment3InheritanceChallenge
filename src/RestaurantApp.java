4ced75978d59a7fed3d90e1916028d0fc00e9371
Kavitha <kavithaa434@gmail.com>
2020-08-10 21:36:54 -0400

    update

--- src/RestaurantApp.java
+++ src/RestaurantApp.java
@@ -6,27 +6,29 @@ public class RestaurantApp {
 		pay(bill);
 		TippableBill tipBill=new TippableBill(8.50,.06,2);
 		pay(tipBill);
-		double res=whatsMyChange(bill,20);
+		double res=whatsMyChange(bill,10);
 		if(res==0) {
-			System.out.println("Not enough money");
+			System.out.printf("Difference=$%.2f",res);
 		}else {
-			System.out.println("Difference="+res);
+			System.out.printf("%s%.2f","Difference=$",res);
 		}
 		
 
 	}
 	private static void pay(Bill bill) {
 		double total=bill.calcTotal();
-		System.out.println("Total="+total+"$");
+		System.out.println("Total=$"+total);
 	}
 	private static double whatsMyChange(Bill b,double value) {
-		double val=0;
+		double val=value-b.calcTotal();
 	
 		if(b.calcTotal()>value) {
-			 val=0.0;
+			 System.out.println("Not enough money");
+			 return val;
 		}else {
-			val=value-b.calcTotal();
-		}return val;
+			return val;
+			
+		}
 	}
 	
 
